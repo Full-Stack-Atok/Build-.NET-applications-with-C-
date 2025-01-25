7c925a4cf504e475fe21e4c86009ec36fad6f18c
Full-Stack-Atok <komsaykerby@gmail.com>
2025-01-25 21:59:20 +0800

    Trying to get back grove

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-// Console.WriteLine("My name is Atok");
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code!");
+﻿// // See https://aka.ms/new-console-template for more information
+// //Console.WriteLine("Hello, World!");
+// // Console.WriteLine("My name is Atok");
+// // Console.Write("Congratulations!");
+// // Console.Write(" ");
+// // Console.Write("You wrote your first lines of code!");
+
+// // Console.WriteLine("Congratulations!");
+// // Console.Write("You wrote your first lines of code!");
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.WriteLine(@"c:\invoices");
+// // Console.WriteLine(@"    c:\source\repos(this is where your code goes)");
+
+// // Format output using verbatim string literals
+
+
+
+// // Unicode escape characters
+
+// // Kon'nichiwa World!
+// // Console.WriteLine("\u3053\u3093\u306b\u3061\u306F World!");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"\invoices\app.exe -j");
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cadc4a0478f432f54b5dba49107f0406a0f1bf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+832a775e81909731b05c680e2ec5ca7dee0536b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
