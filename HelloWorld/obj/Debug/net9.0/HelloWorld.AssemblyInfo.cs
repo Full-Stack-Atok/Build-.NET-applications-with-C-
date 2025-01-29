644b827bb84ece6d9d2ace4bcbd735427d96d027
Full-Stack-Atok <komsaykerby@gmail.com>
2025-01-29 13:55:59 +0800

    First_Project

--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2531e1db23be5644a7bf2fe4cf29d550952f32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3f311e8d3e6ea50ff8c4905ab0694f3879e07f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
