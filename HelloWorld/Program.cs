730f2b15cf875b82c46a04aa11c74b057e75e4e3
Full-Stack-Atok <komsaykerby@gmail.com>
2025-01-30 16:24:07 +0800

    Created a GPA Calculator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,180 +1,236 @@
-﻿// // // // // // See https://aka.ms/new-console-template for more information
+﻿// // // // // // // See https://aka.ms/new-console-template for more information
 
-// // // // // //Console.WriteLine("Hello, World!");
-// // // // // // Console.WriteLine("My name is Atok");
-// // // // // // Console.Write("Congratulations!");
-// // // // // // Console.Write(" ");
-// // // // // // Console.Write("You wrote your first lines of code!");
+// // // // // // //Console.WriteLine("Hello, World!");
+// // // // // // // Console.WriteLine("My name is Atok");
+// // // // // // // Console.Write("Congratulations!");
+// // // // // // // Console.Write(" ");
+// // // // // // // Console.Write("You wrote your first lines of code!");
 
-// // // // // // Console.WriteLine("Congratulations!");
-// // // // // // Console.Write("You wrote your first lines of code!");
+// // // // // // // Console.WriteLine("Congratulations!");
+// // // // // // // Console.Write("You wrote your first lines of code!");
 
-// // // // Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
-// // // // Console.WriteLine("Invoice: 1021\t\tComplete!");
-// // // // Console.WriteLine("Invoice: 1022\t\tComplete!");
-// // // // Console.WriteLine("\nOutput Directory:\t");
-// // // // Console.WriteLine(@"c:\invoices");
-// // // // // // Console.WriteLine(@"    c:\source\repos(this is where your code goes)");
+// // // // // Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+// // // // // Console.WriteLine("Invoice: 1021\t\tComplete!");
+// // // // // Console.WriteLine("Invoice: 1022\t\tComplete!");
+// // // // // Console.WriteLine("\nOutput Directory:\t");
+// // // // // Console.WriteLine(@"c:\invoices");
+// // // // // // // Console.WriteLine(@"    c:\source\repos(this is where your code goes)");
 
-// // // // // // Format output using verbatim string literals
+// // // // // // // Format output using verbatim string literals
 
 
 
-// // // // // // Unicode escape characters
+// // // // // // // Unicode escape characters
 
-// // // // // // Kon'nichiwa World!
-// // // // // // Console.WriteLine("\u3053\u3093\u306b\u3061\u306F World!");
+// // // // // // // Kon'nichiwa World!
+// // // // // // // Console.WriteLine("\u3053\u3093\u306b\u3061\u306F World!");
 
-// // // // // To generate Japanese invoices:
-// // // // // Nihon no seikyū-sho o seisei suru ni wa:
-// // // // Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-// // // // // User command to run an application
-// // // // Console.WriteLine(@"\invoices\app.exe -j");
+// // // // // // To generate Japanese invoices:
+// // // // // // Nihon no seikyū-sho o seisei suru ni wa:
+// // // // // Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// // // // // // User command to run an application
+// // // // // Console.WriteLine(@"\invoices\app.exe -j");
 
-// // // // Perform basic string formatting in C# 
+// // // // // Perform basic string formatting in C# 
 
-// // // // string firstName = "Atok";
-// // // // string message = $"Hello {firstName}!";
+// // // // // string firstName = "Atok";
+// // // // // string message = $"Hello {firstName}!";
 
-// // // // Console.WriteLine(message);
+// // // // // Console.WriteLine(message);
 
-// // // // int version = 11;
-// // // // string updateText = "Update to Windows";
-// // // // Console.WriteLine($"{updateText} {version}");
+// // // // // int version = 11;
+// // // // // string updateText = "Update to Windows";
+// // // // // Console.WriteLine($"{updateText} {version}");
+
+// // // // // string projectName = "ACME";
+// // // // // Console.WriteLine($@"C:\Output\{projectName}\Data");
 
 // // // // string projectName = "ACME";
-// // // // Console.WriteLine($@"C:\Output\{projectName}\Data");
 
-// // // string projectName = "ACME";
+// // // // string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
-// // // string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+// // // // Console.WriteLine($"View English output:\n\tc:\\Exercise\\{projectName}\\Data.txt\n");
+// // // // Console.WriteLine($"{russianMessage}:\n\tc:\\Exercise\\{projectName}\\ru-RU\\Data.txt\n");
 
-// // // Console.WriteLine($"View English output:\n\tc:\\Exercise\\{projectName}\\Data.txt\n");
-// // // Console.WriteLine($"{russianMessage}:\n\tc:\\Exercise\\{projectName}\\ru-RU\\Data.txt\n");
+// // // // Perform basic operations on numbers in C#
 
-// // // Perform basic operations on numbers in C#
+// // // // int firstNumber = 12;
+// // // // int secondNumber = 7;
+// // // // Console.WriteLine(firstNumber + secondNumber);
 
-// // // int firstNumber = 12;
-// // // int secondNumber = 7;
-// // // Console.WriteLine(firstNumber + secondNumber);
+// // // // string firstName = "Bob";
+// // // // int widgetsSold = 7;
+// // // // Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
 
-// // // string firstName = "Bob";
-// // // int widgetsSold = 7;
-// // // Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+// // // // int sum = 7 + 4;
+// // // // int difference = 7 - 5;
+// // // // int product = 7 * 5;
+// // // // int quotient = 7 / 5;
 
-// // // int sum = 7 + 4;
-// // // int difference = 7 - 5;
-// // // int product = 7 * 5;
-// // // int quotient = 7 / 5;
+// // // // Console.WriteLine("Sum: " + sum);
+// // // // Console.WriteLine("Difference: " + difference);
+// // // // Console.WriteLine("Product: " + product);
+// // // // Console.WriteLine("Quotient: " + quotient);
 
-// // // Console.WriteLine("Sum: " + sum);
-// // // Console.WriteLine("Difference: " + difference);
-// // // Console.WriteLine("Product: " + product);
-// // // Console.WriteLine("Quotient: " + quotient);
+// // // // decimal decimalQuotient = 7.0m / 5;
+// // // // Console.WriteLine($"Decimal quotient: {decimalQuotient}");
 
-// // // decimal decimalQuotient = 7.0m / 5;
-// // // Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+// // // int first = 7;
+// // // int second = 5;
+// // // decimal quotient = (decimal)first / (decimal)second;
+// // // Console.WriteLine(quotient);
 
-// // int first = 7;
-// // int second = 5;
-// // decimal quotient = (decimal)first / (decimal)second;
-// // Console.WriteLine(quotient);
+// // // Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+// // // Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
 
-// // Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
-// // Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+// // // int value1 = 3 + 4 * 5;
+// // // int value2 = (3 + 4) * 5;
+// // // Console.WriteLine(value1);
+// // // Console.WriteLine(value2);
 
-// // int value1 = 3 + 4 * 5;
-// // int value2 = (3 + 4) * 5;
-// // Console.WriteLine(value1);
-// // Console.WriteLine(value2);
+// // // Increment and decrement values
 
-// // Increment and decrement values
+// // // int value = 0;
+// // // value = value + 5;
+// // // value += 5;
+// // // Console.WriteLine(value);
 
-// // int value = 0;
-// // value = value + 5;
-// // value += 5;
-// // Console.WriteLine(value);
+// // // int value = 0;
+// // // value = value += 1;
+// // // value++;
+// // // Console.WriteLine(value);
 
-// // int value = 0;
-// // value = value += 1;
-// // value++;
-// // Console.WriteLine(value);
+// // // int value = 1;
 
-// // int value = 1;
+// // // value = value + 1;
+// // // Console.WriteLine("First increment: " + value);
 
-// // value = value + 1;
-// // Console.WriteLine("First increment: " + value);
+// // // value += 1;
+// // // Console.WriteLine("Second increment: " + value);
 
-// // value += 1;
-// // Console.WriteLine("Second increment: " + value);
+// // // value++;
+// // // Console.WriteLine("Third increment: " + value);
 
-// // value++;
-// // Console.WriteLine("Third increment: " + value);
+// // // value = value - 1;
+// // // Console.WriteLine("First decrement: " + value);
 
-// // value = value - 1;
-// // Console.WriteLine("First decrement: " + value);
+// // // value -= 1;
+// // // Console.WriteLine("Second decrement: " + value);
 
-// // value -= 1;
-// // Console.WriteLine("Second decrement: " + value);
+// // // value--;
+// // // Console.WriteLine("Third decrement: " + value);
 
-// // value--;
-// // Console.WriteLine("Third decrement: " + value);
+// // // int value = 1;
+// // // value++;
+// // // Console.WriteLine("First: " + value);
+// // // Console.WriteLine($"Second: {value++}");
+// // // Console.WriteLine("Third: " + value);
+// // // Console.WriteLine("Fourth: " + (++value));
 
-// // int value = 1;
-// // value++;
-// // Console.WriteLine("First: " + value);
-// // Console.WriteLine($"Second: {value++}");
-// // Console.WriteLine("Third: " + value);
-// // Console.WriteLine("Fourth: " + (++value));
+// // int fahrenheit = 94;
 
-// int fahrenheit = 94;
+// // decimal temperature = (fahrenheit - 32) * (5.0m / 9.0m);
+// // Console.WriteLine($"The temperature is {temperature} Celsius.");
 
-// decimal temperature = (fahrenheit - 32) * (5.0m / 9.0m);
-// Console.WriteLine($"The temperature is {temperature} Celsius.");
+// // Guided - Project
 
-// Guided - Project
+// // initialize variables - graded assignments 
+// // initialize variables - graded assignments 
+// // initialize variables - graded assignments 
+// int currentAssignments = 5;
 
-// initialize variables - graded assignments 
-// initialize variables - graded assignments 
-// initialize variables - graded assignments 
-int currentAssignments = 5;
+// int sophia1 = 93;
+// int sophia2 = 87;
+// int sophia3 = 98;
+// int sophia4 = 95;
+// int sophia5 = 100;
 
-int sophia1 = 93;
-int sophia2 = 87;
-int sophia3 = 98;
-int sophia4 = 95;
-int sophia5 = 100;
+// int nicolas1 = 80;
+// int nicolas2 = 83;
+// int nicolas3 = 82;
+// int nicolas4 = 88;
+// int nicolas5 = 85;
 
-int nicolas1 = 80;
-int nicolas2 = 83;
-int nicolas3 = 82;
-int nicolas4 = 88;
-int nicolas5 = 85;
+// int zahirah1 = 84;
+// int zahirah2 = 96;
+// int zahirah3 = 73;
+// int zahirah4 = 85;
+// int zahirah5 = 79;
 
-int zahirah1 = 84;
-int zahirah2 = 96;
-int zahirah3 = 73;
-int zahirah4 = 85;
-int zahirah5 = 79;
+// int jeong1 = 90;
+// int jeong2 = 92;
+// int jeong3 = 98;
+// int jeong4 = 100;
+// int jeong5 = 97;
 
-int jeong1 = 90;
-int jeong2 = 92;
-int jeong3 = 98;
-int jeong4 = 100;
-int jeong5 = 97;
+// int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+// int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+// int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+// int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
 
-int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
-int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
-int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+// decimal sophiaScore = (decimal) sophiaSum / currentAssignments;
+// decimal nicolasScore = (decimal) nicolasSum / currentAssignments;
+// decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
+// decimal jeongScore = (decimal) jeongSum / currentAssignments;
 
-decimal sophiaScore = (decimal) sophiaSum / currentAssignments;
-decimal nicolasScore = (decimal) nicolasSum / currentAssignments;
-decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
-decimal jeongScore = (decimal) jeongSum / currentAssignments;
+// Console.WriteLine("Student\t\tGrade\n");
+// Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
+// Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
+// Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
+// Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
 
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
-Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
-Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
-Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
+
+// Guided project - Calculate final GPA
+
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+int totalCreditHours = 0;
+totalCreditHours += course1Credit;
+totalCreditHours += course2Credit;
+totalCreditHours += course3Credit;
+totalCreditHours += course4Credit;
+totalCreditHours += course5Credit;
+
+int totalGradePoints = 0;
+totalGradePoints += course1Credit * course1Grade;
+totalGradePoints += course2Credit * course2Grade;
+totalGradePoints += course3Credit * course3Grade;
+totalGradePoints += course4Credit * course4Grade;
+totalGradePoints += course5Credit * course5Grade;
+
+decimal gradePointAverage = (decimal) totalGradePoints/totalCreditHours;
+int leadingDigit = (int) gradePointAverage;
+int firstDigit = (int) (gradePointAverage * 10) % 10;
+int secondDigit = (int) (gradePointAverage * 100) % 10;
+
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
+
+Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t\t{course5Grade}\t\t{course5Credit}");
+
+Console.WriteLine($"\nFinal GPA:\t\t\t{leadingDigit}.{firstDigit}{secondDigit}");
+
